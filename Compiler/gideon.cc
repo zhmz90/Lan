273a995208c23bd98e71ee98212f8b0c3fcbef48
Lanting Guo <zhmz90@gmail.com>
2017-07-07 19:19:44 +0800

    simple repl

--- Compiler/gideon.cc
+++ Compiler/gideon.cc
@@ -3,11 +3,31 @@
 
 using namespace std;
 
+class Color{
+  
+};
+
+class Console{
+ public:
+  Console()=default;
+  Console(Color c){
+  }
+  
+
+ private:
+  Color color;
+  std::string welcome;
+};
+
+
 int main(int argc ,char* argv[]){
-  cout<<endl;
-  cout<<"\033[1;32m       Hello, I am Gideon, an interactive artificial consciousness.\033[0m"<<endl;
+  cout<<"\033[1;32m";
+  cout<<endl<<endl;
+  cout<<"       Hello, I am Gideon, an interactive artificial consciousness."<<endl;
   cout<<"You can communicate with me via text now."<<endl;
   cout<<endl;
+  cout<<"\033[0m";
+  cout<<endl;  
   cout<<"\033[1;32mGideon>\033[0m";
   string c;
   while (true){
